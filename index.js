cb904937ecbd8a201c52432329ff809005d31d76
mayle <kopytinnna@gmail.com>
2026-04-07 19:51:54 +0500

    Task 2 done

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.getElementById("modal");
+const btn = document.getElementById("button");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
